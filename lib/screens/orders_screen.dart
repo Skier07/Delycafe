9c0d2392fdb1650618e3f7369ca2b5915e5232ec
Roman <Skier07@mail.ru>
2026-06-24 02:49:52 +0500

    Fix Compare

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -164,7 +164,12 @@ class _OrdersScreenState extends State<OrdersScreen> {
         onRefresh: _loadData,
         child: ListView(
           physics: const AlwaysScrollableScrollPhysics(),
-          padding: const EdgeInsets.all(16),
+          padding: EdgeInsets.fromLTRB(
+            16,
+            16,
+            16,
+            MediaQuery.of(context).padding.bottom + 24,
+          ),
           children: [
             if (user == null)
               const _OrdersEmptyState(
